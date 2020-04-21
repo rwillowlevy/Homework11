e3fdf07f13ae45db2d9fb0ea33fcac530d423b58
Rachel Levy <rwillowlevy@gmail.com>
2020-04-20 22:12:57 -0500

    delete files

--- public/assets/js/store.js
+++
@@ -1,9 +0,0 @@
-const uuidv1 = require('uuidv1');
-
-module.exports = function(request){
-      return newNote = {
-         "title": request.title,
-         "text": request.text,
-         "id": uuidv1()
-   }
-}
--- routes/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-let path = require("path");
-let router = require("express").Router();
-
-router.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname,"../public/notes.html"));
-});
-
-module.exports = router;
